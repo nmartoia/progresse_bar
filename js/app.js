796de489c39b0abfe1f55a9edb50e68ae44c5418
nmartoia <nmartoia@edenschool.fr>
2022-04-15 15:47:24 +0200

    progresse bar augmente

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+let update = document.getElementById('update')
+let int =getRandomInt((13)+3);
+let truc = 0;
+(function loop(){
+    setInterval(function() {
+            update.style.maxWidth= truc+'px'
+            truc++
+       loop();
+   }, 100);
+ })();
+ 
